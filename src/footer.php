57bbd375a5556de957acd6bfee50049bdb27ba17
Tyler-Sapp <sapp.tyler.c@gmail.com>
2025-06-15 19:27:27 -0400

    fixed footer.php links

--- src/footer.php
+++ src/footer.php
@@ -2,8 +2,8 @@
     <div class="container">
       <p>© <?php echo date('Y'); ?> Tyler Sapp</p>
       <div class="social-links">
-        <a href="https://linkedin.com/in/tylersapp" target="_blank">LinkedIn</a>
-        <a href="https://github.com/tylersapp" target="_blank">GitHub</a>
+        <a href="https://www.linkedin.com/in/tyler-c-sapp/" target="_blank">LinkedIn</a>
+        <a href="https://github.com/Tyler-Sapp" target="_blank">GitHub</a>
       </div>
     </div>
   </footer>
